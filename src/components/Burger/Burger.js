ec1cca1e9ac53f8465995fb20710f9a761f05c97
hxue920 <devilsing@gmail.com>
2018-02-12 00:22:32 -0800

    add buildControls and buildControl component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
       .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, idx) => {
           return <BurgerIngredient key={igKey+idx} type={igKey} />
         });
-      });
+      })
+      .reduce((arr, ele) => {
+        return arr.concat(ele);
+      }, []);
+  if (!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
