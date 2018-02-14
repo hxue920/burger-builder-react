77e1a7810768c1a612a11f743c17eec727a294d3
hxue920 <devilsing@gmail.com>
2018-02-13 21:27:20 -0800

    add http post functionality for burger orders using axios

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,12 +4,13 @@ import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientsSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-          <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-          </li> );
-    });
+  .map(igKey => {
+    return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+        </li> );
+  });
+
   return (
     <Fragment>
       <h3>Your Order</h3>
@@ -23,6 +24,5 @@ const orderSummary = (props) => {
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Fragment>
   );
-};
-
+}
 export default orderSummary;
